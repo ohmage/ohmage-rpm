27967a48b25a43b05fe34a61777276de223e59bf
Jeroen <jeroen@jeroen-ubuntu.(none)>
2013-04-23 14:55:20 -0700

    fix typo

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 tar -pczf ohmage-2.14.tar.gz ohmage-2.14
 
 #copy to your home directory
-mkdir -p ohmage.spec ~/rpmbuild/SPECS
+mkdir -p ~/rpmbuild/SPECS
 cp -f ohmage.spec ~/rpmbuild/SPECS
 
-mkdir -p ohmage.spec ~/rpmbuild/SOURCES
+mkdir -p ~/rpmbuild/SOURCES
 cp -f ohmage-server-2.14.tar.gz ~/rpmbuild/SOURCES
 
 #build
