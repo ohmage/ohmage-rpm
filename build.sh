620d543a47d7e681650986eaf7b9f049ac9e0c45
Jeroen <jeroen@jeroen-ubuntu.(none)>
2013-04-23 15:11:51 -0700

    first 2.15 build

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #create the tar.gz
-tar -pczf ohmage-2.14.tar.gz ohmage-2.14
+tar -pczf ohmage-2.15.tar.gz ohmage-2.15
 
 #copy to your home directory
 mkdir -p ~/rpmbuild/SPECS
 cp -f ohmage.spec ~/rpmbuild/SPECS
 
 mkdir -p ~/rpmbuild/SOURCES
-cp -f ohmage-2.14.tar.gz ~/rpmbuild/SOURCES
+cp -f ohmage-2.15.tar.gz ~/rpmbuild/SOURCES
 
 #build
 rpmbuild -ba ~/rpmbuild/SPECS/ohmage.spec
